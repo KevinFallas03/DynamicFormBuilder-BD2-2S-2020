fb8cd276dc6a2077c5ee3847487c961ec320c0ba
Kenneth Sánchez <38081826+KennethS0@users.noreply.github.com>
2020-11-09 21:20:12 -0600

    Lazy Loading Authorization Routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
