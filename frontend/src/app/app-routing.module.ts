27c591d682d5d0384f4444b832134914bd19c847
dcastro18 <dacastro@ic-itcr.ac.cr>
2020-11-10 23:05:31 -0600

    Cambios en el inicio
    Se añade la pagina de incio con la pagina del fomulario

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TemplateComponent } from './template-builder/template-builder.component';
+import { FormComponent } from "./form/form.component";
+import { HomeComponent } from "./home/home.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'crear', component: TemplateComponent },
+  { path: 'form', component: FormComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes),
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
