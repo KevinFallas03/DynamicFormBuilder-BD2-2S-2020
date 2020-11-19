b0967ec9d7594c88de06d06bd369f80c7557ae0b
Esteban Madrigal <ememadrigal.em@gmail.com>
2020-11-19 03:55:54 -0600

    Revert "prod link added"
    This reverts commit 2c7e8a7810b6b91c3f8d14436b10aecc2f355b2d.

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,3 @@
 export const environment = {
-  production: true,
-  url: 'https://dynamicformbuilder.herokuapp.com'
+  production: true
 };
--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  url: 'http://localhost:3000'
+  url: 'https://dynamicformbuilder.herokuapp.com'
 };
 
 /*
