d7c54a9f7e12ba68589b5a62fbef1d98b05b52ee
Esteban Madrigal <ememadrigal.em@gmail.com>
2020-11-20 23:52:15 -0600

    Heroku url added

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-    //url: 'https://dynamicformbuilder.herokuapp.com'
-    url: 'http://localhost:3000'
+    url: 'https://dynamicformbuilder.herokuapp.com'
+    //url: 'http://localhost:3000'
 };
 
 /*
